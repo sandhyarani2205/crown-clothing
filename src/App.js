d119fc629189d30f1674493f4e8fe25667e5eb8b
sandhyarani <sandhyaranihegde2205@gmail.com>
2020-06-28 21:10:06 +0200

    added sign up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
-import {auth} from './firebase/firebase.utils'
+import {auth, createUserProfileDocument, firestore} from './firebase/firebase.utils'
 
 class App extends React.Component{
   constructor(){
@@ -19,10 +19,21 @@ class App extends React.Component{
   unSubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-      console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+      this.setState({ currentUser: userAuth });
+    });
   }
 
   componentWillUnmount(){
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../../components/form-input/form-input.component'
 import CustomButton from '../../components/custom-button/custom-button.component'
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {signInWithGoogle, auth} from '../../firebase/firebase.utils'
 
 
 class SignIn extends React.Component {
@@ -15,9 +15,16 @@ class SignIn extends React.Component {
         };
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
-        this.setState({email:'', password:''})
+        const {email, password} = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email,password)
+            this.setState({email:'', password:''})
+        }catch{
+            console.log('error');
+        }
     }
 
     handleChange = (event) => {
